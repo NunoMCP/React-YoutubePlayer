f6851e4d94c4df025e33427365df6b0492a34c24
NunoMCP <NunoMCP@users.noreply.github.com>
2021-01-11 18:05:45 +0000

    Updates

---
+++ precache-manifest.568d315d8bc7bb573e7ce9723dae76c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0d22483a0480ffacbc7c9527ec3d8c",
+    "url": "/React-YoutubePlayer/index.html"
+  },
+  {
+    "revision": "567dc8556b7b1c2f1946",
+    "url": "/React-YoutubePlayer/static/css/main.b98e3bfa.chunk.css"
+  },
+  {
+    "revision": "3763f2051ea5c562848a",
+    "url": "/React-YoutubePlayer/static/js/2.54894ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-YoutubePlayer/static/js/2.54894ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567dc8556b7b1c2f1946",
+    "url": "/React-YoutubePlayer/static/js/main.9798f7ef.chunk.js"
+  },
+  {
+    "revision": "706a1e904ca3b40be7ed",
+    "url": "/React-YoutubePlayer/static/js/runtime-main.4378d1be.js"
+  }
+]);
